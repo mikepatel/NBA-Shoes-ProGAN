4a77aa4f4a8ece0a1f1cc0521cf53ad5c244a919
Michael Patel <michael.patel94@gmail.com>
2020-04-30 20:17:34 -0400

    Adding training loop

--- train.py
+++ train.py
@@ -57,6 +57,113 @@ def plot_images(images_arr):
     plt.show()
 
 
+# discriminaor loss function
+def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    real_loss = cross_entropy(
+        tf.ones_like(real_output),
+        real_output
+    )
+
+    fake_loss = cross_entropy(
+        tf.zeros_like(fake_output),
+        fake_output
+    )
+
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+
+# generator loss function
+def generator_loss(generated_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+    generated_loss = cross_entropy(
+        tf.ones_like(generated_output),
+        generated_output
+    )
+
+    return generated_loss
+
+
+# generate and save images
+def generate_and_save_images(model, epoch, z_input, save_dir):
+    predictions = model(z_input, training=False)
+    predictions = predictions[:16]  # generate 16 images
+
+    # rescale from [-1, 1] to [0, 1]
+    predictions = (predictions + 1) / 2
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i])
+        plt.axis("off")
+
+    fig_name = os.path.join(save_dir, f'Epoch {epoch:05d}')
+    plt.savefig(fig_name)
+    plt.close()
+
+
+# training loop
+def train(dataset, d, g, d_optimizer, g_optimizer, z_input, save_dir):
+    # training metrics
+    d_train_loss = tf.keras.metrics.Mean("d_train_loss", dtype=tf.float32)
+    g_train_loss = tf.keras.metrics.Mean("g_train_loss", dtype=tf.float32)
+    train_summary_writer = tf.summary.create_file_writer(save_dir)
+
+    for e in range(NUM_EPOCHS):
+        # get a batch
+        num_batches = len(dataset)
+        for i in range(num_batches):
+            batch = dataset[i]
+
+            # noise input for generator
+            noise = tf.random.normal(shape=(BATCH_SIZE, NOISE_DIM))
+
+            # GradientTape
+            with tf.GradientTape() as g_tape, tf.GradientTape() as d_tape:
+                # generator
+                fake_batch = g(noise, training=True)
+
+                # discriminator
+                real_output = d(batch, training=True)
+                fake_output = d(fake_batch, training=True)
+
+                # loss functions
+                g_loss = generator_loss(fake_output)
+                d_loss = discriminator_loss(real_output, fake_output)
+
+            # compute gradients recorded on "tape"
+            g_gradients = g_tape.gradient(g_loss, g.trainable_variables)
+            d_gradients = d_tape.gradient(d_loss, d.trainable_variables)
+
+            # apply gradients to model variables to minimize loss function
+            g_optimizer.apply_gradients(zip(g_gradients, g.trainable_variables))
+            d_optimizer.apply_gradients(zip(d_gradients, d.trainable_variables))
+
+            d_train_loss(d_loss)
+            g_train_loss(g_loss)
+
+        if e % 200 == 0 or e == NUM_EPOCHS - 1:
+            # generate sample output
+            generate_and_save_images(
+                model=g,
+                epoch=e+1,
+                z_input=z_input,
+                save_dir=save_dir
+            )
+
+        # write loss metrics to TensorBoard
+        with train_summary_writer.as_default():
+            tf.summary.scalar("d_loss", d_train_loss.result(), step=e)
+            tf.summary.scalar("g_loss", g_train_loss.result(), step=e)
+
+        # reset metrics every epoch
+        d_train_loss.reset_states()
+        g_train_loss.reset_states()
+
+
 ################################################################################
 # Main
 if __name__ == "__main__":
@@ -96,5 +203,16 @@ if __name__ == "__main__":
     generator.summary()
 
     # ----- TRAINING ----- #
+    z_input_gen = tf.random.normal(shape=(BATCH_SIZE, NOISE_DIM))
+
+    train(
+        dataset=train_data_gen,
+        d=discriminator,
+        g=generator,
+        d_optimizer=discriminator_optimizer,
+        g_optimizer=generator_optimizer,
+        z_input=z_input_gen,
+        save_dir=output_dir
+    )
 
     # ----- SAVE ----- #
