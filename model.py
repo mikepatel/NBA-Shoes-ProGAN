d8c2e28d3efc8d977deb7005306db4b64195a5c5
Michael Patel <michael.patel94@gmail.com>
2020-04-30 20:08:19 -0400

    Adding model defns

--- model.py
+++ model.py
@@ -17,5 +17,60 @@ from parameters import *
 
 ################################################################################
 # Discriminator
+def build_discriminator():
+    model = tf.keras.Sequential()
 
+    # Layer: Conv: 4x4
+    model.add(tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(5, 5),
+        strides=1,
+        input_shape=(IMAGE_WIDTH, IMAGE_HEIGHT, NUM_CHANNELS),
+        padding="same"
+    ))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
+
+    # Layer: Dropout
+    model.add(tf.keras.layers.Dropout(rate=DROPOUT_RATE))
+
+    # Layer: Flatten
+    model.add(tf.keras.layers.Flatten())
+
+    # Layer: Output
+    model.add(tf.keras.layers.Dense(
+        units=1
+    ))
+
+    return model
+
+
+################################################################################
 # Generator
+def build_generator():
+    model = tf.keras.Sequential()
+
+    # Layer: Fully connected
+    model.add(tf.keras.layers.Dense(
+        units=4*4*64,
+        input_shape=(NOISE_DIM, ),
+        use_bias=False
+    ))
+    model.add(tf.keras.layers.BatchNormalization())
+    model.add(tf.keras.layers.LeakyReLU(alpha=LEAKY_ALPHA))
+
+    # Layer: Reshape
+    model.add(tf.keras.layers.Reshape(
+        target_shape=(4, 4, 64)
+    ))
+
+    # Layer: Output: 4x4x3
+    model.add(tf.keras.layers.Conv2DTranspose(
+        filters=3,  # RGB
+        kernel_size=(5, 5),
+        strides=1,
+        padding="same",
+        activation=tf.keras.activations.tanh
+    ))
+
+    return model
